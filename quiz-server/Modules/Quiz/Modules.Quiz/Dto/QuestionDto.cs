509447420262042589092d03f243ab7475c96ae5
Trương Đình Nhật <nhatflame@gmail.com>
2025-05-08 23:06:20 +0700

    feat: add Explanation property to QuestionDto and update QuizMapper

--- quiz-server/Modules/Quiz/Modules.Quiz/Dto/QuestionDto.cs
+++ quiz-server/Modules/Quiz/Modules.Quiz/Dto/QuestionDto.cs
@@ -6,6 +6,7 @@ namespace Modules.Quiz.Dto
         public int Order { get; set; }
         public string QuestionType { get; set; } = string.Empty;
         public string Text { get; set; } = string.Empty;
+        public string Explanation { get; set; } = string.Empty;
         public List<AnswerDto> Answers { get; set; } = new();
     }
 }
